bdd529bf8fad8c5437727952dce07bb8b16ba5a2
Lucca Kaasgaard Christiansen <1078059@ucn.dk>
2021-02-16 23:37:12 +0100

    initial commit

---
+++ domain/src/ship_stats.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, Clone)]
+pub struct ShipStats {
+    shield_hp: usize,
+    armor_hp: usize,
+    hull_hp: usize,
+    velocity: usize,
+    agility: usize,
+    mass: usize,
+    power_grid: usize,
+    cpu: usize,
+}
