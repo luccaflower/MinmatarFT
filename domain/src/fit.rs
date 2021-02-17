4b845264fd75d380e14ce27f3d475f68f5ad9679
Lucca Kaasgaard Christiansen <1078059@ucn.dk>
2021-02-17 17:21:10 +0100

    fixed ship slots collection size

--- domain/src/fit.rs
+++ domain/src/fit.rs
@@ -4,18 +4,21 @@ use crate::ship::Ship;
 #[derive(Debug, Clone)]
 pub struct Fit<'a> {
     ship: &'a Ship,
-    high_slots: Vec<Option<&'a Module>>,
-    med_slots: Vec<Option<&'a Module>>,
-    low_slots: Vec<Option<&'a Module>>,
+    high_slots: Box<[Option<&'a Module>]>,
+    med_slots: Box<[Option<&'a Module>]>,
+    low_slots: Box<[Option<&'a Module>]>,
 }
 
 impl<'a> Fit<'a> {
     pub fn new(ship: &'a Ship) -> Self {
+        fn generate_empty(size: u8) -> Box<[Option<&'a Module>]> {
+            (0..size).into_iter().map(|_|None).collect::<Vec<Option<&'a Module>>>().into_boxed_slice()
+        }
         Self {
             ship,
-            high_slots: Vec::with_capacity(ship.high_slots.clone() as usize),
-            med_slots: Vec::with_capacity(ship.med_slots.clone() as usize),
-            low_slots: Vec::with_capacity(ship.low_slots.clone() as usize),
+            high_slots: generate_empty(ship.high_slots.clone()),
+            med_slots: generate_empty(ship.med_slots.clone()),
+            low_slots: generate_empty(ship.low_slots.clone()),
         }
     }
 }
