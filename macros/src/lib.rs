42138fcb248ecd24d621246c974f642c2ccaf218
Alexandra Østermark <alex.cramt@gmail.com>
2021-02-18 23:14:58 +0100

    sde provider crate

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -3,105 +3,13 @@ mod domain_impl;
 use crate::domain_impl::ship::ShipWrapper;
 use crate::domain_impl::slice::SliceWrapper;
 use proc_macro::TokenStream;
-use sde_parser::ParserArgument;
-use std::env::current_dir;
-use std::fs::{create_dir, File};
-use std::io::Write;
-use std::ops::Deref;
-use std::path::Path;
-use std::{fs, io};
 use tokio::runtime::Runtime;
-
-fn static_dir_path() -> &'static Path {
-    Path::new("__static_data")
-}
-
-async fn download_static_data() {
-    if !static_dir_path().exists() {
-        create_dir(static_dir_path()).unwrap();
-    }
-    let zip_path = static_dir_path().join("sde.zip");
-    if !zip_path.exists() {
-        let download_link =
-            "https://eve-static-data-export.s3-eu-west-1.amazonaws.com/tranquility/sde.zip";
-        let bytes = reqwest::get(download_link)
-            .await
-            .unwrap()
-            .bytes()
-            .await
-            .unwrap();
-        let mut zip_file = File::create(&zip_path).unwrap();
-        zip_file.write_all(bytes.deref()).unwrap();
-        zip_file.flush().unwrap();
-    };
-    if !static_dir_path().join("sde").exists() {
-        let mut archive = zip::ZipArchive::new(File::open(&zip_path).unwrap()).expect("5");
-        for i in 0..archive.len() {
-            let mut file = archive.by_index(i).expect("4");
-            let outpath = match file.enclosed_name() {
-                Some(path) => path.to_owned(),
-                None => continue,
-            };
-
-            let outpath = current_dir().unwrap().join(static_dir_path()).join(outpath);
-            if (&*file.name()).ends_with('/') {
-                fs::create_dir_all(&outpath).expect("3");
-            } else {
-                if let Some(p) = outpath.parent() {
-                    let p = current_dir().unwrap().join(static_dir_path()).join(p);
-                    if !p.exists() {
-                        fs::create_dir_all(&p).expect("2");
-                    }
-                }
-                let mut outfile = fs::File::create(&outpath)
-                    .expect(format!("failed to write to {:?}", outpath).as_str());
-                io::copy(&mut file, &mut outfile).expect("1");
-            }
-        }
-    }
-}
+use sde_provider::SdeProvider;
 
 #[proc_macro]
 pub fn generate_all_data(_: TokenStream) -> TokenStream {
     let runtime = Runtime::new().unwrap();
-    runtime.block_on(download_static_data());
-    let args = ParserArgument::new(
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("typeIDs.yaml"),
-        )
-        .unwrap(),
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("groupIDs.yaml"),
-        )
-        .unwrap(),
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("categoryIDs.yaml"),
-        )
-        .unwrap(),
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("dogmaAttributes.yaml"),
-        )
-        .unwrap(),
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("typeDogma.yaml"),
-        )
-        .unwrap(),
-    );
+    let args = runtime.block_on(SdeProvider::new().cached("__static_data").execute());
     let result = sde_parser::parse(args).unwrap();
     let ships = result
         .into_iter()
---
+++ sde_provider/src/lib.rs
@@ -0,0 +1,135 @@
+use std::borrow::Cow;
+use sde_parser::{ParserArgument, InputSdeData};
+use std::ops::Deref;
+use std::fs::{File, create_dir_all};
+use std::io::{Cursor, Write, Read};
+use std::path::Path;
+use std::env::current_dir;
+use std::{fs, io};
+use zip::read::ZipFile;
+
+pub struct SdeProvider<'a> {
+    cached_dir_name: Option<Cow<'a, str>>,
+    url: Cow<'a, str>,
+}
+
+impl<'a> SdeProvider<'a> {
+    pub fn new() -> Self {
+        Self {
+            cached_dir_name: None,
+            url: Cow::Borrowed("https://eve-static-data-export.s3-eu-west-1.amazonaws.com/tranquility/sde.zip"),
+        }
+    }
+
+    pub fn cached<T: Into<Cow<'a, str>>>(mut self, dir_name: T) -> Self {
+        self.cached_dir_name = Some(dir_name.into());
+        self
+    }
+
+    pub fn url<T: Into<Cow<'a, str>>>(mut self, url: T) -> Self {
+        self.url = url.into();
+        self
+    }
+}
+
+impl SdeProvider<'_> {
+    async fn fetch(&self) -> Vec<u8> {
+        reqwest::get(self.url.deref()).await.unwrap().bytes().await.unwrap().to_vec()
+    }
+    pub async fn execute(self) -> InputSdeData {
+        match &self.cached_dir_name {
+            None => {
+                let bytes = Cursor::new(self.fetch().await);
+                let mut zip = zip::ZipArchive::new(bytes).unwrap();
+
+                fn into(mut z: ZipFile) -> Cursor<Vec<u8>> {
+                    let mut buffer = Vec::with_capacity(z.size() as usize);
+                    z.read_to_end(&mut buffer).unwrap();
+                    Cursor::new(buffer)
+                }
+
+                let type_ids = into(zip.by_name("sde/fsd/typeIDs.yaml").unwrap());
+                let group_ids = into(zip.by_name("sde/fsd/groupIDs.yaml").unwrap());
+                let category_ids = into(zip.by_name("sde/fsd/categoryIDs.yaml").unwrap());
+                let dogma_attributes = into(zip.by_name("sde/fsd/dogmaAttributes.yaml").unwrap());
+                let type_dogma = into(zip.by_name("sde/fsd/typeDogma.yaml").unwrap());
+                ParserArgument::new(type_ids, group_ids, category_ids, dogma_attributes, type_dogma).into()
+            }
+            Some(dir_name) => {
+                let dir = Path::new(dir_name.deref());
+                if !dir.exists() {
+                    create_dir_all(dir).unwrap();
+                }
+                let zip_path = dir.join("sde.zip");
+                if !zip_path.exists() {
+                    let bytes = self.fetch().await;
+                    let mut zip_file = File::create(&zip_path).unwrap();
+                    zip_file.write_all(bytes.as_slice()).unwrap();
+                    zip_file.flush().unwrap();
+                }
+                let extract_dir = dir.join("sde");
+                if !extract_dir.exists() {
+                    let mut archive = zip::ZipArchive::new(File::open(&zip_path).unwrap()).unwrap();
+                    for i in 0..archive.len() {
+                        let mut file = archive.by_index(i).unwrap();
+                        let outpath = match file.enclosed_name() {
+                            Some(path) => path.to_owned(),
+                            None => continue,
+                        };
+
+                        let outpath = current_dir().unwrap().join(&dir).join(outpath);
+                        if (&*file.name()).ends_with('/') {
+                            fs::create_dir_all(&outpath).unwrap();
+                        } else {
+                            if let Some(p) = outpath.parent() {
+                                let p = current_dir().unwrap().join(&dir).join(p);
+                                if !p.exists() {
+                                    fs::create_dir_all(&p).unwrap();
+                                }
+                            }
+                            let mut outfile = fs::File::create(&outpath).unwrap();
+                            io::copy(&mut file, &mut outfile).unwrap();
+                        }
+                    }
+                }
+                ParserArgument::new(
+                    File::open(
+                        dir
+                            .join("sde")
+                            .join("fsd")
+                            .join("typeIDs.yaml"),
+                    )
+                        .unwrap(),
+                    File::open(
+                        dir
+                            .join("sde")
+                            .join("fsd")
+                            .join("groupIDs.yaml"),
+                    )
+                        .unwrap(),
+                    File::open(
+                        dir
+                            .join("sde")
+                            .join("fsd")
+                            .join("categoryIDs.yaml"),
+                    )
+                        .unwrap(),
+                    File::open(
+                        dir
+                            .join("sde")
+                            .join("fsd")
+                            .join("dogmaAttributes.yaml"),
+                    )
+                        .unwrap(),
+                    File::open(
+                        dir
+                            .join("sde")
+                            .join("fsd")
+                            .join("typeDogma.yaml"),
+                    )
+                        .unwrap(),
+                ).into()
+            }
+        }
+    }
+}
