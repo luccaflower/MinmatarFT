dd2c02cceb3e605f458d32f928031ebc7a8fae0a
Alexandra Østermark <alex.cramt@gmail.com>
2021-02-18 15:18:41 +0100

    moved sde parsing to its own crate

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,22 +1,9 @@
 mod domain_impl;
-mod faction_ids;
-mod model;
-mod ship_type_ids;
 
 use crate::domain_impl::ship::ShipWrapper;
 use crate::domain_impl::slice::SliceWrapper;
-use crate::faction_ids::faction_by_id;
-use crate::model::category_id::CategoryId;
-use crate::model::dogma_attribute::DogmaAttribute;
-use crate::model::group_id::GroupId;
-use crate::model::type_dogma::TypeDogma;
-use crate::model::type_id::TypeId;
-use crate::ship_type_ids::ship_type_by_id;
-use fitting_engine::faction::Faction;
-use fitting_engine::ship::Ship;
-use fitting_engine::ship_stats::ShipStats;
 use proc_macro::TokenStream;
-use std::collections::HashMap;
+use sde_parser::ParserArgument;
 use std::env::current_dir;
 use std::fs::{create_dir, File};
 use std::io::Write;
@@ -78,112 +65,47 @@ async fn download_static_data() {
 pub fn generate_all_data(_: TokenStream) -> TokenStream {
     let runtime = Runtime::new().unwrap();
     runtime.block_on(download_static_data());
-    let type_ids = parse_type_ids();
-    let group_ids = parse_group_ids();
-    let category_ids = parse_category_ids();
-    let type_dogmas = parse_type_dogma();
-    let dogma_attributes = parse_dogma_attributes();
-    let (ships, rest_data): (
-        Vec<(TypeId, &GroupId, &CategoryId, Vec<(f64, &DogmaAttribute)>)>,
-        Vec<(TypeId, &GroupId, &CategoryId, Vec<(f64, &DogmaAttribute)>)>,
-    ) = type_ids
+    let args = ParserArgument::new(
+        File::open(
+            static_dir_path()
+                .join("sde")
+                .join("fsd")
+                .join("typeIDs.yaml"),
+        )
+        .unwrap(),
+        File::open(
+            static_dir_path()
+                .join("sde")
+                .join("fsd")
+                .join("groupIDs.yaml"),
+        )
+        .unwrap(),
+        File::open(
+            static_dir_path()
+                .join("sde")
+                .join("fsd")
+                .join("categoryIDs.yaml"),
+        )
+        .unwrap(),
+        File::open(
+            static_dir_path()
+                .join("sde")
+                .join("fsd")
+                .join("dogmaAttributes.yaml"),
+        )
+        .unwrap(),
+        File::open(
+            static_dir_path()
+                .join("sde")
+                .join("fsd")
+                .join("typeDogma.yaml"),
+        )
+        .unwrap(),
+    );
+    let result = sde_parser::parse(args).unwrap();
+    let ships = result
         .into_iter()
-        .map(|(i, x)| {
-            let group = group_ids.get(&x.group_id)?;
-            let category = category_ids.get(&group.category_id)?;
-            let type_dogma = type_dogmas.get(&i)?;
-            let dogma_attributes = type_dogma
-                .dogma_attributes
-                .iter()
-                .map(|x| (x.value.clone(), dogma_attributes.get(&x.attribute_id)))
-                .filter(|(_, x)| x.is_some())
-                .map(|(a, b)| (a, b.unwrap()))
-                .collect::<Vec<(f64, &DogmaAttribute)>>();
-
-            Some((x, group, category, dogma_attributes))
-        })
-        .filter(|x| x.is_some())
-        .map(|x| x.unwrap())
-        .partition(|(_, _, c, _)| c.name.en.as_ref() == Some(&"Ship".to_string()));
-    //panic!("{:?}", ships.iter().filter(|(x,_,_,_)|x.faction_id.is_none()).map(|(x,_,_,_)|x.name.en.clone()).collect::<Vec<Option<String>>>());
-    let ships = ships
-        .into_iter()
-        .map(|(t, g, c, v)| {
-            let ship_type = ship_type_by_id(t.group_id.clone());
-            (t, g, c, v, ship_type)
-        })
-        .filter(|(_, _, _, _, ship_type)| ship_type.is_some())
-        .map(|(t, g, c, v, ship_type)| (t, g, c, v, ship_type.unwrap()))
-        .map(|(t, g, c, v, ship_type)| {
-            let (low_slots, _) = v
-                .iter()
-                .find(|(_, x)| x.name.as_str() == "lowSlots")
-                .expect("1");
-            let (med_slots, _) = v
-                .iter()
-                .find(|(_, x)| x.name.as_str() == "medSlots")
-                .expect("2");
-            let (high_slots, _) = v
-                .iter()
-                .find(|(_, x)| x.name.as_str() == "hiSlots")
-                .expect("3");
-            let (shield_hp, _) = v
-                .iter()
-                .find(|(_, x)| x.name.as_str() == "shieldCapacity")
-                .expect("4");
-            let (armor_hp, _) = v
-                .iter()
-                .find(|(_, x)| x.name.as_str() == "armorHP")
-                .expect("5");
-            let (hull_hp, _) = v.iter().find(|(_, x)| x.name.as_str() == "hp").expect("6");
-            let (velocity, _) = v
-                .iter()
-                .find(|(_, x)| x.name.as_str() == "maxVelocity")
-                .expect("7");
-            let (agility, _) = v
-                .iter()
-                .find(|(_, x)| x.name.as_str() == "agility")
-                .expect("7");
-            let mass = &t.mass.expect("8");
-            let (power_grid, _) = v
-                .iter()
-                .find(|(_, x)| x.name.as_str() == "powerOutput")
-                .expect("9");
-            let (cpu, _) = v
-                .iter()
-                .find(|(_, x)| x.name.as_str() == "cpuOutput")
-                .expect("10");
-
-            let name = t.name.en.expect("11");
-            let faction = match t.faction_id {
-                None => match name.as_str() {
-                    "Whiptail" | "Chameleon" => Faction::Guristas,
-                    "Pacifier" | "Marshal" | "Enforcer" => Faction::Concord,
-                    "Stratios Emergency Responder" => Faction::SistersOfEve,
-                    _ => panic!("couldnt determine faction for ship {} ", name),
-                },
-                Some(x) => faction_by_id(x),
-            };
-            Ship::new(
-                name,
-                ship_type,
-                faction,
-                high_slots.clone() as u8,
-                med_slots.clone() as u8,
-                low_slots.clone() as u8,
-                ShipStats::new(
-                    shield_hp.clone() as usize,
-                    armor_hp.clone() as usize,
-                    hull_hp.clone() as usize,
-                    velocity.clone() as usize,
-                    agility.clone() as usize,
-                    mass.clone() as usize,
-                    power_grid.clone() as usize,
-                    cpu.clone() as usize,
-                ),
-            )
-        })
-        .map(ShipWrapper::new)
+        .map(|(_, x)| ShipWrapper::new(x))
         .collect::<Vec<ShipWrapper>>()
         .into_boxed_slice();
     let length = ships.len();
@@ -192,81 +114,4 @@ pub fn generate_all_data(_: TokenStream) -> TokenStream {
     TokenStream::from(quote::quote! {
         static ALL_SHIPS: [fitting_engine::ship::Ship; #length] = #a;
     })
-}
-
-fn parse_type_ids() -> HashMap<u64, TypeId> {
-    serde_yaml::from_reader::<_, HashMap<u64, TypeId>>(
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("typeIDs.yaml"),
-        )
-        .unwrap(),
-    )
-    .unwrap()
-    .into_iter()
-    .filter(|(_, x)| x.published.clone())
-    .collect::<HashMap<u64, TypeId>>()
-}
-
-fn parse_group_ids() -> HashMap<u64, GroupId> {
-    serde_yaml::from_reader::<_, HashMap<u64, GroupId>>(
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("groupIDs.yaml"),
-        )
-        .unwrap(),
-    )
-    .unwrap()
-    .into_iter()
-    .filter(|(_, x)| x.published.clone())
-    .collect::<HashMap<u64, GroupId>>()
-}
-
-fn parse_category_ids() -> HashMap<u64, CategoryId> {
-    serde_yaml::from_reader::<_, HashMap<u64, CategoryId>>(
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("categoryIDs.yaml"),
-        )
-        .unwrap(),
-    )
-    .unwrap()
-    .into_iter()
-    .filter(|(_, x)| x.published.clone())
-    .collect::<HashMap<u64, CategoryId>>()
-}
-
-fn parse_dogma_attributes() -> HashMap<u64, DogmaAttribute> {
-    serde_yaml::from_reader::<_, HashMap<u64, DogmaAttribute>>(
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("dogmaAttributes.yaml"),
-        )
-        .unwrap(),
-    )
-    .unwrap()
-    .into_iter()
-    .filter(|(_, x)| x.published.clone())
-    .collect::<HashMap<u64, DogmaAttribute>>()
-}
-
-fn parse_type_dogma() -> HashMap<u64, TypeDogma> {
-    serde_yaml::from_reader::<_, HashMap<u64, TypeDogma>>(
-        File::open(
-            static_dir_path()
-                .join("sde")
-                .join("fsd")
-                .join("typeDogma.yaml"),
-        )
-        .unwrap(),
-    )
-    .unwrap()
 }
---
+++ sde_parser/src/lib.rs
@@ -0,0 +1,199 @@
+use crate::faction_ids::faction_by_id;
+use crate::model::category_id::CategoryId;
+use crate::model::dogma_attribute::DogmaAttribute;
+use crate::model::group_id::GroupId;
+use crate::model::type_dogma::TypeDogma;
+use crate::model::type_id::TypeId;
+use crate::ship_type_ids::ship_type_by_id;
+use fitting_engine::faction::Faction;
+use fitting_engine::ship::Ship;
+use fitting_engine::ship_stats::ShipStats;
+use std::collections::HashMap;
+use std::io;
+
+mod faction_ids;
+pub mod model;
+mod ship_type_ids;
+
+#[derive(Debug)]
+pub struct ParserArgument<A, B, C, D, E>
+where
+    A: io::Read,
+    B: io::Read,
+    C: io::Read,
+    D: io::Read,
+    E: io::Read,
+{
+    type_ids: A,
+    group_ids: B,
+    category_ids: C,
+    dogma_attributes: D,
+    type_dogma: E,
+}
+
+impl<A, B, C, D, E> ParserArgument<A, B, C, D, E>
+where
+    A: io::Read,
+    B: io::Read,
+    C: io::Read,
+    D: io::Read,
+    E: io::Read,
+{
+    pub fn new(
+        type_ids: A,
+        group_ids: B,
+        category_ids: C,
+        dogma_attributes: D,
+        type_dogma: E,
+    ) -> Self {
+        Self {
+            type_ids,
+            group_ids,
+            category_ids,
+            dogma_attributes,
+            type_dogma,
+        }
+    }
+}
+
+pub type InputSdeData = (
+    HashMap<u64, TypeId>,
+    HashMap<u64, GroupId>,
+    HashMap<u64, CategoryId>,
+    HashMap<u64, DogmaAttribute>,
+    HashMap<u64, TypeDogma>,
+);
+
+pub type OutputSdeData = (HashMap<String, Ship>);
+
+impl<A, B, C, D, E> Into<InputSdeData> for ParserArgument<A, B, C, D, E>
+where
+    A: io::Read,
+    B: io::Read,
+    C: io::Read,
+    D: io::Read,
+    E: io::Read,
+{
+    fn into(self) -> InputSdeData {
+        (
+            serde_yaml::from_reader::<_, HashMap<u64, TypeId>>(self.type_ids)
+                .unwrap()
+                .into_iter()
+                .filter(|(_, x)| x.published.clone())
+                .collect(),
+            serde_yaml::from_reader::<_, HashMap<u64, GroupId>>(self.group_ids)
+                .unwrap()
+                .into_iter()
+                .filter(|(_, x)| x.published.clone())
+                .collect(),
+            serde_yaml::from_reader::<_, HashMap<u64, CategoryId>>(self.category_ids)
+                .unwrap()
+                .into_iter()
+                .filter(|(_, x)| x.published.clone())
+                .collect(),
+            serde_yaml::from_reader::<_, HashMap<u64, DogmaAttribute>>(self.dogma_attributes)
+                .unwrap()
+                .into_iter()
+                .filter(|(_, x)| x.published.clone())
+                .collect(),
+            serde_yaml::from_reader::<_, HashMap<u64, TypeDogma>>(self.type_dogma).unwrap(),
+        )
+    }
+}
+
+pub fn parse<T: Into<InputSdeData>>(input: T) -> Result<OutputSdeData, Box<dyn std::error::Error>> {
+    let (type_ids, group_ids, category_ids, dogma_attributes, type_dogmas) = input.into();
+    let (ships, _rest_data): (
+        Vec<(
+            TypeId,
+            &GroupId,
+            &CategoryId,
+            HashMap<u64, (f64, &DogmaAttribute)>,
+        )>,
+        Vec<(
+            TypeId,
+            &GroupId,
+            &CategoryId,
+            HashMap<u64, (f64, &DogmaAttribute)>,
+        )>,
+    ) = type_ids
+        .into_iter()
+        .map(|(i, x)| {
+            let group = group_ids.get(&x.group_id)?;
+            let category = category_ids.get(&group.category_id)?;
+            let type_dogma = type_dogmas.get(&i)?;
+            let dogma_attributes = type_dogma
+                .dogma_attributes
+                .iter()
+                .map(|x| {
+                    (
+                        x.attribute_id,
+                        x.value.clone(),
+                        dogma_attributes.get(&x.attribute_id),
+                    )
+                })
+                .filter(|(_, _, x)| x.is_some())
+                .map(|(a, b, c)| (a, (b, c.unwrap())))
+                .collect::<HashMap<u64, (f64, &DogmaAttribute)>>();
+
+            Some((x, group, category, dogma_attributes))
+        })
+        .filter(|x| x.is_some())
+        .map(|x| x.unwrap())
+        .partition(|(_, _, c, _)| c.name.en.as_ref() == Some(&"Ship".to_string()));
+    let ship_map = ships
+        .into_iter()
+        .map(|(t, g, c, v)| {
+            let ship_type = ship_type_by_id(t.group_id.clone());
+            (t, g, c, v, ship_type)
+        })
+        .filter(|(_, _, _, _, ship_type)| ship_type.is_some())
+        .map(|(t, g, c, v, ship_type)| (t, g, c, v, ship_type.unwrap()))
+        .map(|(t, g, c, v, ship_type)| {
+            let (low_slots, _) = v.get(&12).unwrap();
+            let (med_slots, _) = v.get(&13).unwrap();
+            let (high_slots, _) = v.get(&14).unwrap();
+            let (shield_hp, _) = v.get(&263).unwrap();
+            let (armor_hp, _) = v.get(&265).unwrap();
+            let (hull_hp, _) = v.get(&9).unwrap();
+            let (velocity, _) = v.get(&37).unwrap();
+            let (agility, _) = v.get(&70).unwrap();
+            let mass = &t.mass.unwrap();
+            let (power_grid, _) = v.get(&11).unwrap();
+            let (cpu, _) = v.get(&48).unwrap();
+
+            let name = t.name.en.unwrap();
+            let faction = match t.faction_id {
+                //some entries randomly dont have factions attached, so we harded them :shrug:
+                None => match name.as_str() {
+                    "Whiptail" | "Chameleon" => Faction::Guristas,
+                    "Pacifier" | "Marshal" | "Enforcer" => Faction::Concord,
+                    "Stratios Emergency Responder" => Faction::SistersOfEve,
+                    _ => panic!("couldnt determine faction for ship {} ", name),
+                },
+                Some(x) => faction_by_id(x),
+            };
+            Ship::new(
+                name,
+                ship_type,
+                faction,
+                high_slots.clone() as u8,
+                med_slots.clone() as u8,
+                low_slots.clone() as u8,
+                ShipStats::new(
+                    shield_hp.clone() as usize,
+                    armor_hp.clone() as usize,
+                    hull_hp.clone() as usize,
+                    velocity.clone() as usize,
+                    agility.clone() as usize,
+                    mass.clone() as usize,
+                    power_grid.clone() as usize,
+                    cpu.clone() as usize,
+                ),
+            )
+        })
+        .map(|x| (x.name.to_string(), x))
+        .collect();
+
+    Ok(ship_map)
+}
